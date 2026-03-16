3d87fd70b95a909ce04777dad9440bce805f6542
Eugen Natuchchi <me@eugen.berlin>
2026-03-16 03:05:13 +0100

    Add a bit more meaning to this chaos

--- app/controllers/achievements_controller.rb
+++ app/controllers/achievements_controller.rb
@@ -1,4 +1,7 @@
 class AchievementsController < ApplicationController
   def index
+    @chains = Chain.includes(:game, chain_nodes: :achievement).order(created_at: :desc)
+    @total_chains = @chains.size
+    @total_achievements = @chains.sum { |chain| chain.chain_nodes.size }
   end
 end
