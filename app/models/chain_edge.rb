d1ac5d19b2c8250151b26bd3841e6c5e9dc3006d
Eugen Natuchchi <me@eugen.berlin>
2026-03-16 02:31:46 +0100

    Revert "Make ChainEdge's nodes optional"
    This reverts commit ac0c98be530fa554d00065844c4032f6e23450df.

--- app/models/chain_edge.rb
+++ app/models/chain_edge.rb
@@ -3,6 +3,6 @@ class ChainEdge < ApplicationRecord
 
   belongs_to :achievement, optional: true, foreign_key: :ref_id
 
-  belongs_to :from, optional: true, class_name: "ChainNode", foreign_key: :from_node
-  belongs_to :to, optional: true, class_name: "ChainNode", foreign_key: :to_node
+  belongs_to :from, class_name: "ChainNode", foreign_key: :from_node
+  belongs_to :to, class_name: "ChainNode", foreign_key: :to_node
 end
